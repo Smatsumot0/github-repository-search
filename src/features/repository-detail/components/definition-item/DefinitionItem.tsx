2a04e1b1bb66aab8a16b315d7aa84b7d9c553b77
Smatsumot0 <killer0xxxx@gmail.com>
2026-05-10 17:36:45 +0900

    DefinitionItemコンポーネント作成

---
+++ src/features/repository-detail/components/definition-item/DefinitionItem.tsx
@@ -0,0 +1,26 @@
+import styles from "./DefinitionItem.module.css"
+import clsx from "clsx"
+
+type DefinitionItemProps = {
+  term: string
+  children: React.ReactNode
+  className?: string
+  termHidden?: boolean
+}
+
+export function DefinitionItem({
+  term,
+  children,
+  className,
+  termHidden = false,
+}: DefinitionItemProps) {
+  return (
+    <div className={clsx(styles.root, className)}>
+      <dt className={clsx(styles.term, termHidden && "visually-hidden")}>
+        {term}
+      </dt>
+      <dd className={styles.description}>{children}</dd>
+    </div>
+  )
+}
+
