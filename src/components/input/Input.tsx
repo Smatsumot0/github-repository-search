cb3488160d7ff73f333d2a018a9fff17781ce02d
Smatsumot0 <killer0xxxx@gmail.com>
2026-05-11 16:50:04 +0900

    Inputコンポーネント type指定

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -3,8 +3,21 @@ import { ComponentPropsWithoutRef } from "react"
 
 import styles from "./Input.module.css"
 
-type InputProps = ComponentPropsWithoutRef<"input">
+type InputType =
+  | "text"
+  | "search"
+  | "email"
+  | "password"
+  | "url"
+  | "tel"
+  | "number"
 
-export function Input({ className, ...props }: InputProps) {
-  return <input className={clsx(styles.input, className)} {...props} />
+type InputProps = Omit<ComponentPropsWithoutRef<"input">, "type"> & {
+  type?: InputType
+}
+
+export function Input({ className, type = "text", ...props }: InputProps) {
+  return (
+    <input type={type} className={clsx(styles.input, className)} {...props} />
+  )
 }
